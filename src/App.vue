2db8676f6dca56de5abb077052273edbc0f09801
Szalai Szabolcs <sz.szalai16@gmail.com>
2022-03-06 21:04:03 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+    </nav>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+
+
+</style>
