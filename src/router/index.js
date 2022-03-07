7e1843bc9554e368aaf08353d6aea08870310830
Szalai Szabolcs <sz.szalai16@gmail.com>
2022-03-07 22:09:13 +0100

    termékek listázása

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomePageView from '../views/HomePageView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'HomePage',
+      component: HomePageView
     }
   ]
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,13 @@ module.exports = {
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
+  //https://tailwindcss.com/docs/adding-custom-styles#customizing-your-theme
   theme: {
-    extend: {},
+    extend: {
+      /* colors: {
+        'cool-gray': '#1a1a1a',
+      } */
+    },
   },
   plugins: [],
 }
