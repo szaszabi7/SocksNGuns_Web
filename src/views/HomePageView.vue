3a7722fe464fc5195dbff2665acc45cc1006dc25
Szalai Szabolcs <sz.szalai16@gmail.com>
2022-04-16 02:04:14 +0200

    footer fix

--- src/views/HomePageView.vue
+++ src/views/HomePageView.vue
@@ -5,15 +5,17 @@ import Footer from '../components/Footer.vue';
 </script>
 
 <template>
-	<Navbar />
-	<div class="container mx-auto px-10">
-        <div class="grid lg:grid-cols-5 mt-20">
-            <div class="lg:col-span-3 lg:col-start-2">
-                <div class="grid gap-5 justify-center lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2">
-                    <ProductCard />
+    <div class="h-screen flex flex-col">
+        <Navbar />
+        <div class="container mx-auto px-10 pt-24 pb-10 grow">
+            <div class="grid lg:grid-cols-5">
+                <div class="lg:col-span-3 lg:col-start-2">
+                    <div class="grid gap-5 justify-center lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2">
+                        <ProductCard />
+                    </div>
                 </div>
             </div>
         </div>
+        <Footer />
     </div>
-    <Footer />
 </template>
